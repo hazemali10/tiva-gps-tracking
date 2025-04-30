995d385cfda2f3db97f3cede4989af7512c0d148
Amen Hany <amen.hany.2810@gmail.com>
2025-05-01 00:01:42 +0300

    BROKEN: wrote a broken main function that switched LED based on switches

--- APP/main.c
+++ APP/main.c
@@ -1,5 +1,30 @@
+#include "gpio.h"
 #include "tm4c123gh6pm.h"
 
-int main(void) {
 
+int main(void) {
+    GPIO_initPortF();
+    
+    while (1)
+    {
+        if (GPIO_getSwitchValue(SW1) == GPIO_SW_ON)
+        {
+            GPIO_setLedValue(LED_RED, GPIO_LED_ON);
+            GPIO_setLedValue(LED_BLUE, GPIO_LED_OFF);
+            GPIO_setLedValue(LED_GREEN, GPIO_LED_OFF);
+        } 
+        else if (GPIO_getSwitchValue(SW2) == GPIO_SW_ON)
+        {
+            GPIO_setLedValue(LED_RED, GPIO_LED_OFF);
+            GPIO_setLedValue(LED_BLUE, GPIO_LED_ON);
+            GPIO_setLedValue(LED_GREEN, GPIO_LED_OFF);
+        }
+        else
+        {
+            GPIO_setLedValue(LED_RED, GPIO_LED_OFF);
+            GPIO_setLedValue(LED_BLUE, GPIO_LED_OFF);
+            GPIO_setLedValue(LED_GREEN, GPIO_LED_ON);
+        }
+    }
+    
 }
