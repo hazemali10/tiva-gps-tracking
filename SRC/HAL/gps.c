8d95d8434bb97f081ebede09126bf128cbe7c9f2
ayael-emam <ayaamerfarais52@gmail.com>
2025-05-04 22:55:19 +0300

    part one of GPS until Formating GPS Output

--- SRC/HAL/gps.c
+++ SRC/HAL/gps.c
@@ -1,1 +1,15 @@
 #include "gps.h"
+#include "bit_utils.h"
+#include <math.h>
+#define PI  3.14159265358979323846
+const double EARTH_RADIUS =6371000;
+
+float ToDegree(float angle){
+int degree = (int) angle /100;
+float minutes = angle -(float) degree*100;
+return (degree + (minutes/60));
+}
+
+float ToRad(float angle){
+return angle * PI /180;
+}
