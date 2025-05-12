d717205b46b5b2938ee83fa6c61bf6c607c57ed5
ayael-emam <ayaamerfarais52@gmail.com>
2025-05-12 17:21:50 +0300

    Some adjustments in GPS functions

--- SRC/HAL/gps.c
+++ SRC/HAL/gps.c
@@ -8,11 +8,11 @@
 
 #define PI  3.14159265358979323846
 
-char GPS_logName[]="$GPRMS,";
+char GPS_logName[]="$GPRMC,";
 
 //data fields 
 char valid;
-char latitude[15];
+char latitude[15] = "";
 char NS;
 char longitude[15];
 char EW;
@@ -22,17 +22,17 @@ float currentLat;
 float currentLong;
 
 
-void GPS_read(){
+void GPS_read() {
 
-    char counter = 0 ; // number of gps data fields (after each comma)
-    char i = 0 ; //char iterating through logname 
+    char counter = 0;               // number of gps data fields (after each comma)
+    char i = 0;                     // char iterating through logname 
     char recievedChar;
-    char flag = 1 ;//raised after each character data field 
+    char flag = 1;                  // raised after each character data field 
 
     do {
-        while (UART0_getChar() != GPS_logName[i]) 
+        while (UART2_getChar() != GPS_logName[i]) 
         {
-             // until a matching char is caught 
+            // until a matching char is caught 
         }
         i++;
     } while (i != 7);  //Making sure GPS_LogName is correct and complete 
@@ -41,7 +41,7 @@ void GPS_read(){
     while (1) {
 
         if (flag) { //if the received character is a comma it means the end of a data field has been reached so the counter is incremented.
-            recievedChar = UART0_getChar(); // after each char flag = 1 to increment counter  
+            recievedChar = UART2_getChar(); // after each char flag = 1 to increment counter  
             if (recievedChar == ',') {
                 counter++;
             }
@@ -49,28 +49,28 @@ void GPS_read(){
 
         switch (counter) {
             case 1:
-                valid = UART0_getChar();
+                valid = UART2_getChar();
                 break;
             case 2:
-                UART0_receiveString(latitude, ',');
+                UART2_receiveString(latitude, ',');
                 counter++;
                 flag = 0; // after each string flag = 0 counter is already incremented 
                 break;
             case 3:
-                NS = UART0_getChar();
+                NS = UART2_getChar();
                 flag = 1; // after each char flag = 1 to increment counter 
                 break;
             case 4:
-                UART0_receiveString(longitude, ',');
+                UART2_receiveString(longitude, ',');
                 counter++;
                 flag = 0;
                 break;
             case 5:
-                EW = UART0_getChar();
+                EW = UART2_getChar();
                 flag = 1;
                 break;
             case 6:
-                UART0_receiveString(speed, ',');
+                UART2_receiveString(speed, ',');
                 counter++;
                 flag = 0;
                 break;
@@ -81,14 +81,14 @@ void GPS_read(){
     }
     if(valid == 'A') { // Valid
         if(NS =='N')
-            currentLat = atof(latitude);
+            currentLat = atof(latitude)/100;
         else
-            currentLat = -atof(latitude);
+            currentLat = -atof(latitude)/100;
         
         if(EW =='E') 
-            currentLong = atof(longitude);
+            currentLong = atof(longitude)/100;
         else
-            currentLong = -atof(longitude);
+            currentLong = -atof(longitude)/100;
     }
 }
 
@@ -107,7 +107,7 @@ float GPS_getDistance (float currentLong , float currentLat , float destLong , f
     float currentLongRad= toRad(currentLong);
     float currentLatRad= toRad(currentLat);
     float destLongRad= toRad(destLong);
-    float destLatRad= toRad(destLat);
+    float destLatRad=toRad(destLat);
 
 
     //Get Difference
@@ -118,6 +118,6 @@ float GPS_getDistance (float currentLong , float currentLat , float destLong , f
     //Calculate Distance 
     float a = pow(sin(LatDiff/2),2) + cos(currentLatRad)*cos(destLatRad)* pow(sin(LongDiff/2),2); //Haversine Formula
     double c = 2 * atan2(sqrt(a),sqrt(1-a));
-    return 950000 *c;
+    return 600000 *c;
 
 }
