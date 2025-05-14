ac974e44e4faf9fac569655b8e003c7922920e4a
Zeyad <ziad.yasser.yousef@gmail.com>
2025-05-14 23:55:15 +0300

    Reworked LCD driver functions

--- INC/HAL/lcd.h
+++ INC/HAL/lcd.h
@@ -1,11 +1,17 @@
 #ifndef LCD_H
 #define LCD_H
-// lcd functions
+
+typedef enum {
+    UPPER_LINE = 0x80,
+    LOWER_LINE = 0xC0,
+    INCREMENT_CURSOR = 0x06,
+    CLEAR = 0x01
+} LCD_COMMAND;
+
 void delay(long d);
-void LCD_printData(unsigned char data);
-void LCD_data(unsigned char data);
+
+void LCD_pulse(void);
 void LCD_cmd(unsigned char cmd);
-void LCD_string(unsigned char *str,unsigned char len);
 void LCD_init(void);
-void LCD_gpioInit();
+
 #endif
